eec7c60c8f13ef2c77c7eb401cca6f221192e078
rosagi-png <roharts@gmail.com>
2026-08-22 22:54:44 +0800

    if Input.is_action_just pressed() and is_on_floor():

--- scenes/chararcters/player.gd
+++ scenes/chararcters/player.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 var direction_x: float
 @export var speed := 120
-var gravity = 400
+var gravity = 600
 
 
 func get_input():
 	direction_x = Input.get_axis("left", "right")
-	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -250
 	
 func _physics_process(delta: float) -> void:
 	get_input()
