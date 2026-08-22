fb93dcc19a130ea7837d3ebfe260473140faf89a
rosagi-png <roharts@gmail.com>
2026-08-22 21:44:12 +0800

    project folder and config file rest (new 4.6)
    opened with 4.7 new download

--- scenes/chararcters/player.gd
+++ scenes/chararcters/player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-
 var direction_x: float
-@export var speed := 100
-var gravity = 300
+@export var speed := 120
+var gravity = 400
 
 
 func get_input():
@@ -15,4 +14,4 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction_x * speed
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		move_and_slide()
+	move_and_slide()
