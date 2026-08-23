9a499ef285400d966ed11733c63961d43b37e405
rosagi-png <53995169+rosagi-png@users.noreply.github.com>
2026-08-23 17:20:06 +0800

    intro character animation
    //sprite node not found etc

--- scenes/chararcters/player.gd
+++ scenes/chararcters/player.gd
@@ -5,14 +5,31 @@ var direction_x: float
 var gravity = 600
 
 
-func get_input():
-	direction_x = Input.get_axis("left", "right")
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -250
-	
+
 func _physics_process(delta: float) -> void:
 	get_input()
+	move(delta)
+	animation()
+	move_and_slide()
+	
+	
+func get_input(): 
+	pass
+	
+	
+	
+
+func move(delta):
 	velocity.x = direction_x * speed
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	move_and_slide()
+	
+	direction_x = Input.get_axis("left", "right")
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -250
+
+func animation():
+#legs
+#if not facing left, face left
+	if direction_x != 0:
+		$Sprites/LegSprite.flip_h = direction_x < 0
