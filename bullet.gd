cd7055d1f74468212ba8dc50c7533de7a2d43ff3
rosagi-png <roharts@gmail.com>
2026-09-11 04:14:59 +0800

    interim bullet direction - snap

--- bullet.gd
+++ bullet.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 var direction: Vector2
 var speed: int = 200
+const OFFSET = 16
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 	
 func setup(pos: Vector2, dir: Vector2):
-		position = pos 
+		position = pos + dir * OFFSET
 		direction = dir
+		
+#normalize raw vector (mouse position)
+var raw_dir = get_local_mouse_position().normalized()
+var adjusted_dir = Vector2i(round(raw_dir.x), round(raw_dir.y))
